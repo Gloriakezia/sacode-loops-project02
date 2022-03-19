14cb8c0f475fcb154b4a78d0470c88b9e5bcd421
Gloria Kezia <91767970+Gloriakezia@users.noreply.github.com>
2022-03-19 10:11:47 +0900

    push it

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+// while
+while (true){
+//  // Do somethings
+//}
+// Loop with $counter
+//$counter = 0;
+//$loop = true;
+//while ($loop) {
+//  echo "Counter $counter" . '<br>';
+//  if ($counter) >= 10 {
+//      $loop = false;
+//  };
+//  $counter++;
+//}
+
+// do - while
+$counter = 10;
+//do {
+//  echo "Counter $counter";
+//} while ($counter < 10);
+
+//for
+//for  ($i = 1, $i < 10; $i++){
+//  echo "Counter: $i".'<br>';
+//}
+
+//foreach
+$fruits = ["Banana", "Apple", "Orange"];
+foreach ($fruits) as
+
+
+
+
+
+
+
+?>
